143ccea61e3219d9435f85ef1ce9cdeec21c1232
MichalS <m.sobolewski@netland.pl>
2022-05-23 22:57:08 +0200

    add solve implementation for new task

---
+++ CodeWars/kyu8/TrafficLightTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.kyu8
+{
+    [TestFixture]
+    public class TrafficLightTest
+    {
+        [TestCase("green", "yellow")]
+        [TestCase("yellow", "red")]
+        [TestCase("red", "green")]
+        public void BasicTests(string s, string expected)
+        {
+            Assert.That(TrafficLight.UpdateLight(s), Is.EqualTo(expected));
+        }
+    }
+
+    public class TrafficLight
+    {
+        public static string UpdateLight(string current)
+        {
+            var result = current switch
+            {
+                "green" => "yellow",
+                "yellow" => "red",
+                "red" => "green",
+                _ => "Something went wrong!"
+            };
+
+            return result;
+        }
+    }
+}

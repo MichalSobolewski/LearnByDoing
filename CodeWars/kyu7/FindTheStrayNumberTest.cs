bb1b236e9cf90123fea104703db8062db7077907
MichalS <m.sobolewski@netland.pl>
2022-05-23 22:06:20 +0200

    Add project files.

---
+++ CodeWars/kyu7/FindTheStrayNumberTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.kyu7
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SimpleArray1()
+        {
+            Assert.AreEqual(2, Solution.Stray(new int[] { 1, 1, 2 }));
+        }
+    }
+
+    class Solution
+    {
+        public static int Stray(int[] numbers)
+        {
+            var numberGroups = numbers.GroupBy(x => x);
+            var result = numberGroups.Where(x => x.Count() == 1).Select(x => x.Key);
+            return 0;
+        }
+    }
+}

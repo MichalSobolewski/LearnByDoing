0eba3d2f1cec10eb71a06e3005ce0ac301fe4acf
MichalS <m.sobolewski@netland.pl>
2022-05-23 22:24:48 +0200

    add diffrent and shorter solve

---
+++ CodeWars/kyu7/FindTheStrayNumberTest2.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.kyu7
+{
+    [TestFixture]
+    public class FindTheStrayNumberTest2
+    {
+        [Test]
+        public void SimpleArray1()
+        {
+            Assert.AreEqual(2, FindTheStrayNumber2.Stray(new int[] { 1, 1, 2 }));
+        }
+    }
+
+    public class FindTheStrayNumber2
+    {
+        public static int Stray(int[] numbers)
+        {
+            return numbers.First(x => numbers.Count(c => c == x) == 1);
+        }
+    }
+}

747471f84643d51b195767c6aa79ed5025d46053
MichalS <m.sobolewski@netland.pl>
2022-05-23 23:21:51 +0200

    add solution for kata

---
+++ CodeWars/kyu8/IsHeGonnaSurviveTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.kyu8
+{
+    [TestFixture]
+    class IsHeGonnaSurviveTest
+    {
+        [TestCase(10, 5)]
+        [TestCase(100, 40)]
+        public void ATrueHero(int bullets, int dragons)
+        {
+            Assert.IsTrue(IsHeGonnaSurvive.Hero(bullets, dragons));
+        }
+
+        [TestCase(4, 5)]
+        [TestCase(1500, 751)]
+        [TestCase(0, 1)]
+        [TestCase(7, 4)]
+        public void AFalseHero(int bullets, int dragons)
+        {
+            Assert.IsFalse(IsHeGonnaSurvive.Hero(bullets, dragons));
+        }
+    }
+    class IsHeGonnaSurvive
+    {
+        public static bool Hero(int bullets, int dragons) => bullets >= dragons * 2 ? true: false;
+    }
+}

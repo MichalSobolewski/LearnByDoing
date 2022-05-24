b89285e4d11f0369f5c5ee53c6730e591d0dc8e1
MichalS <m.sobolewski@netland.pl>
2022-05-25 00:03:57 +0200

    add the solving of another task

---
+++ CodeWars/kyu7/ShortestWordTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.kyu7
+{
+    //https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+    [TestFixture]
+    public class ShortestWordTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(3, ShortestWord.FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Assert.AreEqual(3, ShortestWord.FindShort("turns out random test cases are easier than writing out basic ones"));
+            Assert.AreEqual(2, ShortestWord.FindShort("Let's travel abroad shall we"));
+        }
+    }
+
+    public class ShortestWord
+    {
+        public static int FindShort(string s) => s.Split(" ").Select(x => x.Length).Min();
+    }
+}
